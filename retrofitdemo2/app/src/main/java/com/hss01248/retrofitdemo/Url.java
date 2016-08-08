cbe9d34cec9948c35f9763c594ba869e814b976c
hss01248 <hss01248@gmail.com>
2016-08-08 20:44:33 +0800

    delete

--- retrofitdemo2/app/src/main/java/com/hss01248/retrofitdemo/Url.java
+++
@@ -1,9 +0,0 @@
-package com.hss01248.retrofitdemo;
-
-/**
- * Created by Administrator on 2016/8/6 0006.
- */
-public class Url {
-    public static final String HEAD = "http://test.qxinli.com/api.php?s=/";
-    public static final String USER_LOGIN = "user/login";
-}
